8f7a51ef5d10fed7afd6c05157003b50884ea5ff
lkhankpwr-creator <lkhankpwr@gmail.com>
2025-12-09 19:31:01 -0800

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,20 @@
+import one
+
+print("مساحت دایره")
+a =float(input("enter your rabius:"))
+print( one.area_of_a_cirele(a))
+
+print("مساحت مثلث")
+namber1 =int(input("enter your rulex:"))
+namber2 =int(input("enter your height:"))
+print(one.area_of_a_triangle(namber1,namber2))
+
+print("مساحت مستطیل")
+namber_1 = int(input("enter your length:"))
+namber_2 = int(input("enter your width:"))
+print(one.area_of_the_rectangle(namber_1,namber_2))
+
+print("مساحت مربع")
+namber__1 =int(input("enter your oneside:"))
+namber__2 =int(input(" enter your herself:"))
+print(one.area_of_the_rectangle(namber__1,namber__2))
---
+++ one.py
@@ -0,0 +1,19 @@
+import math
+def area_of_a_cirele (rabius):
+    area1 = math.pi* rabius
+    return area1
+
+
+def area_of_a_triangle(rulex,height):
+    area2 = rulex *height/2
+    return area2
+
+
+def area_of_the_rectangle(length,width):
+    area3 = length * width
+    return area3
+
+
+def square_area (oneside,herself):
+    area4 = oneside * herself
+    return area4
